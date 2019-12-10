192da330e8dfece541be550a20e3a6c532a11004
Brian Nicholls <nichol88@gmail.com>
2019-12-10 03:29:26 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
-
-
+def my_collect(array)
+  i = 0
+  r = []
+  while i < array.length
+    yield array[i]
+    if array[i].include?(" ")
+      r.push(array[i].split(" ").first)
+    else
+      r.push(array[i].upcase)
+    end
+    i += 1
+  end
+  r
+end
